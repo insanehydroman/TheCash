df4857e5ae77111bad24c7a145a6bc19b47d2fce
insanehydroman <insanehydroman@gmail.com>
2022-05-26 22:48:30 -0500

    Created a 2d Animator Controller

--- Assets/Scripts/ThirdPersonMover.cs
+++ Assets/Scripts/ThirdPersonMover.cs
@@ -6,8 +6,13 @@ public class ThirdPersonMover : MonoBehaviour
     [SerializeField] float _turnSpeed = 1000f;
     [SerializeField] private float _moveSpeed = 5;
     private Rigidbody _rb;
+    Animator _animator;
 
-    private void Awake() => _rb = GetComponent<Rigidbody>();
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
 
 
         void Update()
@@ -20,10 +25,15 @@ public class ThirdPersonMover : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= 2;
 
         var velocity = new Vector3(horizontal, 0, vertical);
         velocity *= _moveSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * velocity;
         _rb.MovePosition(transform.position + offset);
+        
+        _animator.SetFloat("Vertical", vertical, 0.1f, Time.fixedDeltaTime);
+        _animator.SetFloat("Horizontal", horizontal, 0.1f, Time.fixedDeltaTime);
     }
 }
