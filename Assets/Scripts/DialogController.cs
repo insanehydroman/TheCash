ef57b883f6bb3e1864186c7847bfeb9a0c381596
insanehydroman <insanehydroman@gmail.com>
2022-05-29 14:20:42 -0500

    Created Dialog Controller

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField] TextAsset _dialog;
+    [SerializeField] TMP_Text _storyText;
+    [SerializeField] Button[] _choiceButtons;
+    
+    Story _story;
+    
+    [ContextMenu("Start Dialog")]
+    public void StartDialog()
+    {
+        _story = new Story(_dialog.text);
+        RefreshView();
+    }
+
+    void RefreshView()
+    {
+        StringBuilder storyTextBuilder = new StringBuilder();
+        while (_story.canContinue)
+            storyTextBuilder.AppendLine(_story.Continue());
+
+        _storyText.SetText(storyTextBuilder);
+
+        for (int i = 0; i < _choiceButtons.Length; i++)
+        {
+            var button = _choiceButtons[i];
+            button.gameObject.SetActive(i < _story.currentChoices.Count);
+            button.onClick.RemoveAllListeners();
+            if (i < _story.currentChoices.Count)
+            {
+                var choice = _story.currentChoices[i];
+                button.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                button.onClick.AddListener(() =>
+                {
+                    _story.ChooseChoiceIndex(choice.index);
+                    RefreshView();
+                });
+            }
+        }
+    }
+}
